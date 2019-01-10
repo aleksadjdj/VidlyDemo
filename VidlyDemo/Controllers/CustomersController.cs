d16b965b7a84d9aa89aeb677ca7b2658a23b93ad
Aleksandar Djurdjević <aleksa.djdj@gmail.com>
2019-01-10 17:37:32 +0100

    21.

---
+++ VidlyDemo/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyDemo.Models;
+
+namespace VidlyDemo.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers = new List<Customer>
+        {
+            new Customer { Name = "Amanda Stevic", Id = 1 },
+            new Customer { Name = "Aleksandar Djurdjevic", Id = 2 },
+        };
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            if (id < 1 || id > _customers.Count)
+                return HttpNotFound();
+
+            return View(_customers[id-1]);
+
+
+        }
+
+    }
+}
